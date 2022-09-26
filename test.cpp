469320b6b0e93e80d0d7841942ec211d3bffe807
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-09-26 17:54:20 -0600

    Cpp test

---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // iostream -> header file for basic input and output functions
+using namespace std;
+int main () {
+    std::cout << "i like pizza" << std::endl;
+    std::cout << "its rlly good" << std::endl;
+    return 0; // exit code zero
+}
