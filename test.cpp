466ac17a16b7bbbe0b5e673484defb0f3efc0bbb
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-09-26 18:27:31 -0600

    string

--- test.cpp
+++ test.cpp
@@ -1,7 +1,31 @@
 #include <iostream> // iostream -> header file for basic input and output functions
 using namespace std;
 int main () {
-    std::cout << "i like pizza" << std::endl;
-    std::cout << "its rlly good" << std::endl;
+    std:: cout << "we are creating variables" << std::endl;
+    int x; // declaration
+    x = 5; // assignemnt
+
+    int y = 6;
+    int sum = x + y;
+
+    std::cout << sum << std::endl;  
+    double price = (double) 2 / 5;
+
+    std::cout <<  price << std::endl; 
+
+    // single char
+    char grade = 'A';
+    char initial = 'B';
+    std::cout << "grade is " << grade <<  " initial is "  << initial << std::endl;
+
+    // boolean
+    bool status = true;
+    std:cout << "status is " << status << std::endl;
+
+    // string
+    std::string name = "Park Paul";
+    std::cout << name;
+
     return 0; // exit code zero
+
 }
