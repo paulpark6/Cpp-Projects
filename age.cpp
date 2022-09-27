66db6b8efd6a7615890e9fed0ba5646d075d60fa
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-09-27 01:07:58 -0600

    age.cpp

--- age.cpp
+++ age.cpp
@@ -5,9 +5,35 @@ int main() {
     cout << "What is your age?";
     cin >> age;
     if (age >= 18) {
-        cout << "Welcome to the site";
+        cout << "Welcome to the site" << endl;
     } else {
         cout << "no!";
+    }
+
+    // switch case
+    char grade;
+    cout << "What is your grade?" << endl;
+    cin >> grade;
+    switch (grade) {
+    case 'A':
+        cout << "Great Job!" << endl;
+        break;
+    case 'B':
+        cout << "Good" << endl;
+        break;
+    case 'C':
+        cout << "Ok" << endl;
+        break;
+    case 'D':
+        cout << "No" << endl;
+        break;
+    case 'F':
+        cout << "Damn,,," << endl;
+        break;
+    
+    default:
+        cout << "Enter (A - F)" << endl;
+        break;
     }
     return 0;
 }
