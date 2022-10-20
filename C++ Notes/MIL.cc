2a4edbb09b12db972d3e398f55f5f509b457c0f1
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-10-19 18:34:38 -0600

    update

--- C++ Notes/MIL.cc
+++ C++ Notes/MIL.cc
@@ -6,6 +6,7 @@ using namespace std;
 struct Student {
     int assns = 0 , mt = 0, final = 0;
     const int id;
+    Student (const Student & other) : assns {other.assns}, mt{other.mt}, final{other.final}, id{other.id} {}
     // constructor for giving user defined values (MIL)
     Student (int assns, int mt, int final, int id) : assns{assns}, mt{mt}, final{final},id{id} {}
 };
@@ -14,6 +15,8 @@ struct Student {
 
 int main () {
     Student s1 = {1,2,3,20949359};
+    Student s2 = {s1};
     cout << s1.id << endl;
+    cout << s2.id << endl;
     return 0;
 }
