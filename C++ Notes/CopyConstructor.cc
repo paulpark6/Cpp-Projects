22b8dbb0feae1d10b75e7eb2a95627c81264c591
paulpark6 <96594926+paulpark6@users.noreply.github.com>
2022-10-25 04:05:15 -0600

    copy constructor
    FIXED ERROR

--- C++ Notes/CopyConstructor.cc
+++ C++ Notes/CopyConstructor.cc
@@ -5,13 +5,21 @@ using namespace std;
 struct Node {
     int data;
     Node *next;
-    Node (const Node & other): data {other.data}, next{other.next} {}
+    // default constructor
+    Node (int data, Node *n = nullptr);
+    // copy constructor
+    Node (const Node* other): data {other->data}, next{other->next} {}
+    // copy assignment
+    // destructor
+    ~Node () {
+        delete(next);
+    }
 };
 
 int main () {
-    Node *n = new Node {new Node {new Node {nullptr}}};
+    Node *n = new Node {1, new Node {2, new Node {3, nullptr}}};
     Node m = *n;
     Node *p = new Node{*n};
-    delete(p);
+    delete(p); 
     return 0;
 }
